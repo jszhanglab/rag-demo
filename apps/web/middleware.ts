90dadf0384bc36380e3d74cce15d7c074e61cb2a
jason zhang <jszhang.lab@gmail.com>
2025-11-08 09:35:27 +0900

    add i18n support

---
+++ apps/web/middleware.ts
@@ -0,0 +1,20 @@
+//middleware.ts is a built-in feature of Next.js that runs before any route or page.
+//It’s used for authentication, i18n, redirects, logging, etc.
+//The matcher defines which paths it applies to.
+//It’s called “middleware” because it sits in the middle between the incoming request and your route handlers — not a dispatcher.
+
+import createMiddleware from "next-intl/middleware";
+import { locales, defaultLocale } from "./i18n/i18n";
+
+/**
+ * This is function from next-intl library
+ */
+export default createMiddleware({
+  locales,
+  defaultLocale,
+  localeDetection: true,
+});
+
+export const config = {
+  matcher: ["/((?!_next|.*\\..*|api).*)"],
+};
